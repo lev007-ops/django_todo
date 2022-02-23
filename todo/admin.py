209f023b5120c5897af3457f8806af579e4eefd0
Lev <lev_bariakh@icloud.com>
2022-02-23 18:40:33 +0500

    added api

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,15 @@
+from turtle import title
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'date', 'done']
+    list_display_links = ['id', 'title']
+    search_fields = ['id', 'title', 'description']
+    list_editable = ['done']
+    list_filter = ['done']
+
+
+admin.site.register(Todo, TodoAdmin)
--- todoapp/settings.py
+++ todoapp/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'todo.apps.TodoConfig'
 ]
 
 MIDDLEWARE = [
@@ -108,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Yekaterinburg'
 
 USE_I18N = True
 
